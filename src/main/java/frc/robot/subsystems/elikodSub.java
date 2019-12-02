64f75b1c4c79d616fe6743ef2d3d3f89f97b1648
snir3034PRO <snir3034pro@gmail.com>
2019-12-02 18:20:42 +0200

    i hate git hub

--- src/main/java/frc/robot/subsystems/elikodSub.java
+++ src/main/java/frc/robot/subsystems/elikodSub.java
@@ -15,7 +15,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class elikodSub extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
-  WPI
+// i hate git hub
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
